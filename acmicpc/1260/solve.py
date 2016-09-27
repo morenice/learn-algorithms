91cd37e4f71901dae4e4e6797fdcc271e20b1fd5
morenice <hyoungguyo@hotmail.com>
2016-09-28 01:28:16 +0900

    add problem 1260 (ing)

---
+++ acmicpc/1260/solve.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+
+def BFS():
+    """Breadth First Search : queue"""
+    pass
+
+
+def DFS():
+    """Depth First Search : stack"""
+    pass
+
+
+if __name__ == '__main__':
+    inputline = input().split(' ')
+    vert = int(inputline[0])
+    trunk = int(inputline[1])
+    start = int(inputline[2])
